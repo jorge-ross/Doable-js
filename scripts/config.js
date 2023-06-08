a7b35df6176c703393781eb5d25da97d18217f4b
Jorge Rosano <jorg.ross.13@gmail.com>
2023-06-08 11:44:11 -0600

    add: utils

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,4 @@
-export const tokenKey = "doable_token";
+export const BASE_URI = "https://doable-api.herokuapp.com/";
 export const appKey = "doable";
+export const tokenKey = "doable_token";
 export const root = document.querySelector("#root");
