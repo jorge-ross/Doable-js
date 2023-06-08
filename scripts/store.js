dff6a89269cfba92dd7c75a4a558905320d267b4
Jorge Rosano <jorg.ross.13@gmail.com>
2023-06-08 12:32:13 -0600

    chore: fix STORE

--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,25 @@
+import { fromLocalStorage, saveLocalStorage } from "./utils.js";
+
 const STORE = {
+  currentPage: fromLocalStorage("current-page") || "login",
   user: null,
   todos: [],
+  setUser(data) {
+    this.user = data;
+  },
+  setCurrentPage(page) {
+    saveLocalStorage("current-page", page);
+    this.currentPage = page;
+  },
+  setTodos(todos) {
+    this.todos = todos;
+    saveLocalStorage("Todos", todos);
+  },
+  addTodo(todo) {
+    this.todo.push(todo);
+  },
+  deleteTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  },
 };
 export default STORE;
